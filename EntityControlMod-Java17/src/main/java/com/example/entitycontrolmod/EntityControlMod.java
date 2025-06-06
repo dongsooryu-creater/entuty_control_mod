f988d941af6bc50cfe94f299b80b0b091006f2c6
dongsooryu-creater <ryudongsoo@outlook.com>
2025-06-06 19:25:30 +0900

    Add files via upload

---
+++ EntityControlMod-Java17/src/main/java/com/example/entitycontrolmod/EntityControlMod.java
@@ -0,0 +1,10 @@
+package com.example.entitycontrolmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("entitycontrolmod")
+public class EntityControlMod {
+    public EntityControlMod() {
+        System.out.println("EntityControlMod loaded!");
+    }
+}
