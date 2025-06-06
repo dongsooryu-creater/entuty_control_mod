e1c677ab7d8db32c48e411703884884baceab7af
dongsooryu-creater <ryudongsoo@outlook.com>
2025-06-06 20:10:35 +0900

    Update EntityControlMod.java

--- EntityControlMod-Java17/src/main/java/com/example/entitycontrolmod/EntityControlMod.java
+++ EntityControlMod-Java17/src/main/java/com/example/entitycontrolmod/EntityControlMod.java
@@ -1,10 +1,18 @@
-package com.example.entitycontrolmod;
+package com.yourname.entitycontrolmod;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 @Mod("entitycontrolmod")
 public class EntityControlMod {
+
     public EntityControlMod() {
-        System.out.println("EntityControlMod loaded!");
+        // 모드 초기화 코드
+    }
+
+    @SubscribeEvent
+    public void onClientSetup(FMLClientSetupEvent event) {
+        // 클라이언트 설정 초기화
     }
 }
